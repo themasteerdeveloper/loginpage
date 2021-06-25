61ea17f05f5630789bb86ac7273f8a11a99b08db
themasteerdeveloper <86499419+themasteerdeveloper@users.noreply.github.com>
2021-06-25 19:18:38 -0300

    Add files via upload

---
+++ src/main/java/pt/espacodosautores/SpringBootRegistrationLoginApplication.java
@@ -0,0 +1,13 @@
+package pt.espacodosautores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRegistrationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRegistrationLoginApplication.class, args);
+	}
+
+}
